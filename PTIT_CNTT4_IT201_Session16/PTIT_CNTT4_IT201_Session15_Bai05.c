d5ab1364a456d639fba980b5d17a4ada73e182c0
Đặng Văn Luyện <133982516+LuyenVPN@users.noreply.github.com>
2025-07-15 02:40:14 +0700

    PTIT_CNTT4_IT201_Session15

---
+++ PTIT_CNTT4_IT201_Session16/PTIT_CNTT4_IT201_Session15_Bai05.c
@@ -0,0 +1,67 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct Node {
+    int data;
+    struct Node *next;
+}Node;
+typedef struct Queue {
+    Node *front;
+    Node *rear;
+}Queue;
+Queue *createQueue() {
+    Queue *q= (Queue*)malloc(sizeof(Queue));
+    q->front=NULL;
+    q->rear=NULL;
+    return q;
+}
+Node *createNode() {
+    int n;
+    printf("nhap so:");
+    scanf("%d",&n);
+    Node *newNode= (Node*)malloc(sizeof(Node));
+    newNode->data=n;
+    newNode->next=NULL;
+    return newNode;
+}
+void enQueue(Queue *q) {
+    Node *newNode=createNode();
+    if (q->rear==NULL) {
+        q->front=newNode;
+        q->rear=newNode;
+    }else {
+        q->rear->next=newNode;
+        q->rear=newNode;
+    }
+}
+void printQueue(Queue *q) {
+    Node *temp=q->front;
+    while (temp!=NULL) {
+        printf("%d ",temp->data);
+        temp=temp->next;
+    }
+}
+void deQueue(Queue *q) {
+    if (q->front == NULL) {
+        printf("queue is empty\n");
+        return;
+    }
+    Node *temp = q->front;
+    printf("\n lay %d ra khoi hang doi.\n", temp->data);
+    q->front = q->front->next;
+    if (q->front == NULL) {
+        q->rear = NULL;
+    }
+    free(temp);
+}
+
+int main() {
+    Queue *myQueue= createQueue();
+    enQueue(myQueue);
+    enQueue(myQueue);
+    enQueue(myQueue);
+    printQueue(myQueue);
+    deQueue(myQueue);
+    printQueue(myQueue);
+    return 0;
+}
